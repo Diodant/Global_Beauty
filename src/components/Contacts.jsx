58690f64680f46eb9d688f1e4153058dedfbf877
Nick Tolok <97984634+Diodant@users.noreply.github.com>
2025-02-25 00:18:33 +0200

    add

--- src/components/Contacts.jsx
+++ src/components/Contacts.jsx
@@ -22,9 +22,13 @@ const Contacts = () => {
           <h2 className="about-title">Контакты</h2>
           <h1 className="secondary-title mb-20">Be In Touch</h1>
           <div className='contact-card'>
-          <p className="contact-info"><strong>Email:</strong> globalbeautyleader2020@gmail.com</p>
-          <p className="contact-info"><strong>Телефон:</strong> +7 (926) 227-09-57</p>
-          <p className="contact-info"><strong>Адрес:</strong> Москва, ул. Радищева 34</p>
+            <p className="contact-info">Организатор Международной Премии Global Beauty Leader:
+            Всероссийская Общественная Организация «Сообщество Предприятий индустрии красоты» (СПИК)</p>
+          <p className="contact-info"><strong className='bold'>Email:</strong> aspikrussia@gmail.com</p>
+          <p className="contact-info"><strong className='bold'>Телефон:</strong> +7 (909) 337 09-96</p>
+          <p className="contact-info"><strong className='bold'>Адрес:</strong> Адрес: 413 115, Саратовская Область, город Энгельс, ул. Смоленская, дом 23</p>
+          <p className="contact-info"><strong className='bold'>Официальный сайт СПИК:</strong> https://spikrussia.ru/</p>
+          <p className="contact-info"><strong className='bold'>Электронная почта для подачи заявок на участие в Международной Премии Global Beauty Leader: </strong> globalbeautyleader2020@gmail.com</p>
           </div>
         </div>
       </div>
